d8675c88bde9efe42eb909e2af85cadb6daf6191
yuchen <109605191+yuchenz03@users.noreply.github.com>
2023-10-09 05:39:59 +0400

    updated database

--- website/models.py
+++ website/models.py
@@ -2,48 +2,19 @@ from . import db #Import all from db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
     
-class Goals(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    goaltype = db.Column(db.String(50))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-class Times(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    event = db.Column(db.String(10000))
-    time = db.Column(db.String(10000))
-    competition = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-class Journal(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    entry = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-class Attendance(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    entry = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-class Questionnaire(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    sleep = db.Column(db.Integer())
-    stress = db.Column(db.Integer())
-    fatigue = db.Column(db.Integer())
-    color = db.Column(db.Integer())
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-class Exercise(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    reps = db.Column(db.Integer())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
+class SwimmerSession(db.Model):
+    #sessionID
+    #userID
+    attendance = db.Column(db.Boolean, default=False, nullable=False)
+    absenceReason = db.Column(db.String(500))
+    sessionJournal = db.Column(db.String(1000))
+    sessionGoal = db.Column(db.String(1000))
+    stress = db.column(db.Integer)
+    sleep = db.column(db.Integer)
+    fatigue = db.column(db.Integer)
+    urineColour = db.column(db.Integer)   
+    
+    
 class User(db.Model, UserMixin): #Creating the user model
     id = db.Column(db.Integer, primary_key=True) #This userID is the primary key and unique to each user
     forename = db.Column(db.String(150))
@@ -51,19 +22,32 @@ class User(db.Model, UserMixin): #Creating the user model
     email = db.Column(db.String(150), unique=True) #unique=True ensures that no users have duplicate emails and usernames
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now()) #The date and time when this is stored is saved as a new column
-    squads_id = db.Column(db.Integer, db.ForeignKey('squads.id'))
-    squad = db.relationship('Squads', back_populates='user')
-    role = db.Column(db.String(7))
-    goals = db.relationship('Goals')
-    journal = db.relationship('Journal')
-    times = db.relationship('Times')
-    attendance = db.relationship('Attendance')
-    questionnaire = db.relationship('Questionnaire')
-    exercise = db.relationship('Exercise')
+    squadID = db.Column(db.Integer)
+    seasonGoal = db.Column(db.String(1000))
+    yearlyGoal = db.Column(db.String(1000))
+    gender = db.Column(db.String(150))
+    birthday = db.Column(db.String(150))
+    nationality = db.Column(db.String(150))
+    formStroke = db.Column(db.String(150))
 
 class Squads(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     squadName = db.Column(db.String(150), unique=True)
-    squadCode = db.Column(db.String(4), unique=True)
-    user = db.relationship('User', back_populates='squad')
 
+class SquadCoaches(db.Model):
+    ...
+    #squadID
+    #coachID
+
+class CompetitionRecord(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    #userID
+    timeSwam = db.Column(db.Float)
+    #competitionID
+    finaPoints = db.Column(db.Integer)
+    
+class Competition(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    
